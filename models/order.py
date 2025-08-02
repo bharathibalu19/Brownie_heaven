53c04f519a78f3c3b015b300a0bed5bec03137ec
BharathiB <bharathibalachandhiran@gmail.com>
2025-08-03 01:12:32 +0530

    Delete models directory

--- models/order.py
+++
@@ -1,8 +0,0 @@
-from . import db
-
-class Order(db.Model):
-    __tablename__ = 'order'
-
-    id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
-    status = db.Column(db.String(50), default='Pending')
